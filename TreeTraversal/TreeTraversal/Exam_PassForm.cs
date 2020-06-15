843f92381eb6a1e1aefe396abfcc27c78570a9fb
Nikita <Shokorovn@gmail.com>
2020-06-15 12:11:15 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/Shok1r/TreeTraversal""
    This reverts commit 3976dd9a39ca48c9e7d00b6832c9c4eabe90026f.

---
+++ TreeTraversal/TreeTraversal/Exam_PassForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeTraversal
+{
+   
+    public partial class Exam_PassForm : Form
+    {
+        
+        public Exam_PassForm(int Errors)
+        {
+            
+            InitializeComponent();
+            textBox1.Text = Errors.ToString();
+        }
+
+        private void Exam_Pass_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
